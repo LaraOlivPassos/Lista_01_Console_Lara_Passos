2d7b064bfa377404cbe5a20c2edef487309b0ee0
LaraOlivPassos <laraolivpassos@gmail.com>
2025-09-09 10:39:53 -0300

    Exercício 10 - Aprovado ou não nas provas

---
+++ Exercício 10/Exercício 10/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Nota da primeira prova:");
+decimal nota1 = decimal.Parse(Console.ReadLine()!);
+Console.WriteLine("Nota da segunda prova:");
+decimal nota2 = decimal.Parse(Console.ReadLine()!);
+decimal media = (nota1 + nota2) / 2;
+Console.WriteLine($"A média entre as suas notas é = {media}");
+if (media >= 6)
+{
+    Console.WriteLine("Dessa forma, parabéns, você foi APROVADO");
+}
+else
+{
+    Console.WriteLine("Dessa forma, infelizmente você foi REPROVADO");
+}
