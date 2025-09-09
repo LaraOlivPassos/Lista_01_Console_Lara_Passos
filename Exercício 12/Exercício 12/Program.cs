d83af7c9560f6780d64c7796b0d0b1bc80bc6433
LaraOlivPassos <laraolivpassos@gmail.com>
2025-09-09 11:06:07 -0300

    Exercício 12 - Maior valor

--- Exercício 12/Exercício 12/Program.cs
+++ Exercício 12/Exercício 12/Program.cs
@@ -2,16 +2,15 @@
 int valor1 = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Valor 2 é"); 
 int valor2 = int.Parse(Console.ReadLine()!);
+if (valor2 == valor1)
+{
+    Console.WriteLine("Este valor já foi apresentado anteriormente, verifique os valores e tente novamente");
+}
 if (valor1 > valor2)
 {
     Console.WriteLine($"O maior valor é {valor1}");
 }
-else if (valor2 > valor1)
+else
 {
     Console.WriteLine($"O maior valor é {valor2}");
 }
-else
-{
-    Console.WriteLine("Os valores são iguais");
-}
-
