d302bb214d41963d95692540b6c291e44a050a41
LaraOlivPassos <laraolivpassos@gmail.com>
2025-09-09 10:30:09 -0300

    Exercício 9 - Valor maçãs

---
+++ Exercício 9/Exercício 9/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Olá! Seja bem vindo a mercearia!");
+Console.WriteLine("As maçãs custam: \nR$1,30 cada se forem compradas menos de uma dúzia\nR$1,0 se forem compradas pelo menos 12");
+Console.WriteLine("Para iniciar, me diga: Quantas maças foram compradas?");
+int quant = int.Parse(Console.ReadLine()!); 
+if (quant < 12)
+{
+    double total = quant * 1.30;
+    Console.WriteLine($"O total a pagar é de R${total}");
+}
+else
+{
+    double total = quant * 1.0;
+    Console.WriteLine($"O total a pagar é de R${total}");
+}
