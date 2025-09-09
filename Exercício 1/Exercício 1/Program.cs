8709b2a1c009be0b37f027604a4e1daa51150505
LaraOlivPassos <laraolivpassos@gmail.com>
2025-09-09 10:11:14 -0300

    Exercício 1 - Trocar A e B

--- Exercício 1/Exercício 1/Program.cs
+++ Exercício 1/Exercício 1/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Olá, vamos iniciar a primeira atividade entre os 6 exercícios!");
+﻿Console.WriteLine("Olá, vamos iniciar a primeira atividade entre os 6 exercícios!");
 int a = 10, b = 20, c;
 Console.WriteLine($"O valor de A é: {a}");
 Console.WriteLine($"O valor de B é: {b}");
