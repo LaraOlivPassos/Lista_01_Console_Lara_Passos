7f900b3614bb2d2bcdcaf7a5e2acaeb9852cb934
LaraOlivPassos <laraolivpassos@gmail.com>
2025-09-09 10:20:01 -0300

    Exercício 6 - Ajuste de salário

--- Exercício 6/Exercício 6/Program.cs
+++ Exercício 6/Exercício 6/Program.cs
@@ -1,13 +1,12 @@
-﻿decimal salarioAtual, percentualAumento, salarioAjust;
-//iniciação
+﻿//Variaveis:
+decimal salarioAtual, percentualAumento, salarioAjust;
+//iniciação:
 Console.WriteLine("Seja bem vindo a Calculadora de salário!\nPara começar, insira o valor do salário atual");
 salarioAtual = decimal.Parse(Console.ReadLine()!);
 Console.WriteLine("Agora, insira o percentual de aumento (sem o símbolo de %)");
 percentualAumento = decimal.Parse(Console.ReadLine()!);
-
-// Cálculo do salário ajustado
+// Cálculo do salário ajustado:
 salarioAjust = salarioAtual + (salarioAtual * (percentualAumento / 100));
-
-//Finalização
+//Finalização:
 Console.WriteLine($"O salário ajustado é = R${salarioAjust}. \n " +
     $"Aumento de R$:{salarioAjust - salarioAtual} em relação ao salário anterior");
