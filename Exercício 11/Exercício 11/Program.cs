3fd375ee82519801bc100a07764fdd439aa94a79
LaraOlivPassos <laraolivpassos@gmail.com>
2025-09-09 10:52:16 -0300

    Exercício 11 - Liberação para votar

---
+++ Exercício 11/Exercício 11/Program.cs
@@ -0,0 +1,16 @@
+﻿int anoAtual = DateTime.Now.Year;
+Console.WriteLine("Em que ano você nasceu?");
+int anoNascimento = int.Parse(Console.ReadLine()!);
+int idade = anoAtual - anoNascimento;
+if (idade >= 18)
+{
+    Console.WriteLine($"Você tem {idade} anos. É obrigado a votar!");
+}
+else if (idade > 16 & idade < 18)
+{
+    Console.WriteLine($"Você tem {idade} anos. Já pode votar, mas não é obrigado (facultativo)");
+}
+else
+{
+    Console.WriteLine($"Você tem {idade} anos. Ainda não pode votar!");
+}
