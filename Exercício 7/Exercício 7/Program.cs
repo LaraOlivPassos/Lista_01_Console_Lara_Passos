6dfef6da8518853e51b62ee4d574238a7e386359
LaraOlivPassos <laraolivpassos@gmail.com>
2025-09-09 10:10:02 -0300

    Exercício 7 -  >, < ou = a 10

---
+++ Exercício 7/Exercício 7/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Escreva um valor para que descubramos se é maior ou menor que 10");
+decimal valor = decimal.Parse(Console.ReadLine()!); // 
+if (valor > 10)
+{
+    Console.WriteLine("O valor é maior que 10");
+}
+else if (valor < 10)
+{
+    Console.WriteLine("O valor é menor que 10");
+}
+else
+{
+    Console.WriteLine("O valor é igual a 10");
+}
