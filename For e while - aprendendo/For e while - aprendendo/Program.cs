9de02871c9d34e6a749508cc849217f96c36d02c
LaraOlivPassos <laraolivpassos@gmail.com>
2025-09-17 08:14:42 -0300

    Aprendendo For e While

---
+++ For e while - aprendendo/For e while - aprendendo/Program.cs
@@ -0,0 +1,26 @@
+﻿int num, resultado; 
+Console.WriteLine("Digite um número para a tabuada");
+num = int.Parse (Console.ReadLine ());
+resultado = 0;
+
+
+int contador = 1;
+while (contador <= 10; )
+{
+    resultado = contador * num;
+    Console.WriteLine($"{num} X {contador} = {resultado}");
+    contador++
+}
+
+
+
+
+
+#region ESTRUTURA FOR
+for (int contador = 1; contador<= 10; contador ++)
+{
+    resultado = contador * num;
+    Console.WriteLine($"{num} X {contador} = {resultado}");
+}
+#endregion
+
