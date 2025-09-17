b14add6445995b50e85812932bd9362a8f02d4e5
LaraOlivPassos <laraolivpassos@gmail.com>
2025-09-17 08:32:51 -0300

    Exercício 14 - Sequência de inteiros

---
+++ Exercício 14/Exercício 14/Program.cs
@@ -0,0 +1,8 @@
+﻿int num;
+Console.WriteLine("Digite o maior número inteiro");
+num = int.Parse(Console.ReadLine());
+
+for (int contador = 1; contador <= num; contador++)
+{
+    Console.Write($"{contador}, ");
+}
