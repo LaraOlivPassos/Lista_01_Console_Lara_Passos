856ccd83e6234eead56d9c652adb515e733bcbb6
LaraOlivPassos <laraolivpassos@gmail.com>
2025-09-17 11:10:09 -0300

    Base do programa

---
+++ Mádia das corridas mata-mata/Mádia das corridas mata-mata/Program.cs
@@ -0,0 +1,13 @@
+﻿//Iniciação do programa
+Console.WriteLine("Olá, seja muito bem vindo a calculadora de médias de tempo da STEM Racing!");
+Console.WriteLine("Quatro corridas serão contabilizadas");
+// Menu de opções
+Console.WriteLine("Menu: \na - Média com tempo de reação(tempo do piloto) \nb - Média sem tempo de reação(tempo do piloto)");
+string escolha = Console.ReadLine()!.ToLower();
+if (escolha == "a")
+{
+
+    Console.WriteLine("Tempo do carro:");
+    double tempoCarro = double.Parse(Console.ReadLine()!);
+
+}
