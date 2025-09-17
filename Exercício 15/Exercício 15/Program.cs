b7b24048c34b8f26fc9dc40934e08c4a9152e0cd
LaraOlivPassos <laraolivpassos@gmail.com>
2025-09-17 08:49:56 -0300

    Exercício 15 - Num. / 11 % ==5

---
+++ Exercício 15/Exercício 15/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Vamos verificar os valores que entre 1000 e 2000, quando divididos por 11 produzem resto = 5"); 
+int etapa = 1000;
+while (etapa <= 2000)
+{
+    if(etapa % 11 == 5)
+    {
+        Console.Write($"{etapa}, ");
+    }
+    etapa++;
+}
